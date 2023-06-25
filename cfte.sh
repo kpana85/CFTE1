5a9b2bc7c31ae39f15ef69888ca08f7cd9936b40
kpana85 <solomonanumel@yahoo.com>
2023-06-25 13:18:19 -0400

    git init

---
+++ cfte.sh
@@ -0,0 +1,13 @@
+mkdir -p ctfe/welcome-to-lab/code
+echo ctfe/welcome-to-lab/code/code3.sh
+echo "peter love romance" > ctfe/welcome-to-lab/code/code3.sh
+echo "peter love romance" > ctfe/welcome-to-lab/code/code3.sh
+echo "peter love his side chick and test" >> ctfe/welcome-to-lab/code/code3.sh
+echo "peter love his side chick and test" >> ctfe/welcome-to-lab/code/code3.sh
+echo "peter love his side chick and test" >> ctfe/welcome-to-lab/code/ccode4.shsh
+echo "peter love his side chick and test" >> ctfe/welcome-to-lab/code/code2.sh
+echo "peter love his side chick and test" >> ctfe/welcome-to-lab/code/code2.sh
+echo "peter love his side chick and test" >> ctfe/welcome-to-lab/code/code4.sh
+echo "peter love his side chick and test" >> ctfe/welcome-to-lab/code/code3.sh
+echo "peter love his side chick and test" >> ctfe/welcome-to-lab/code/code3.sh
+echo "peter love his side chick and test" >> ctfe/welcome-to-lab/code/code3.sh
